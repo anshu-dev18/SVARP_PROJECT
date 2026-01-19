62140262c37279ac96260750b0ceeb938d5a64e9
Anshu <try.anshu18@gmail.com>
2026-01-19 14:24:07 +0530

    Update layouts, drawables, and resources

--- app/src/main/java/com/example/svarp/Medicine.java
+++ app/src/main/java/com/example/svarp/Medicine.java
@@ -1,24 +1,24 @@
 package com.example.svarp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+
 
 public class Medicine extends AppCompatActivity {
+    private MaterialButton btnAnalyze;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_medicine);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        btnAnalyze = findViewById(R.id.btnAnalyze);
+
+        btnAnalyze.setVisibility(View.GONE);
     }
 }
